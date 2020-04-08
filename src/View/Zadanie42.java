da987d35b37725f3ba1ecb2d75e6049fa63db680
Przemysław Galicki <przemyslaw.galicki@edu.wsti.pl>
2020-04-08 19:46:18 +0200

    zad 4.1 Gzip2

--- src/View/Zadanie42.java
+++ src/View/Zadanie42.java
@@ -11,7 +11,6 @@ public class Zadanie42 extends JPanel {
 
     private JButton btn01;
     private JButton btn02;
-
     private Zadanie42ButtonListener listener;
 
     public Zadanie42() {
@@ -20,7 +19,6 @@ public class Zadanie42 extends JPanel {
         btn02 = new JButton("ungZip");
         add(btn01);
         add(btn02);
-
         btn01.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
@@ -44,65 +42,47 @@ public class Zadanie42 extends JPanel {
             public void btn01Clicked() {
                 System.out.println("bt01 Clicked");
                 JFileChooser fc = new JFileChooser();
-
                 if (fc.showOpenDialog(null) == fc.APPROVE_OPTION) {
-
                     try {
                         InputStream istream = new FileInputStream(fc.getSelectedFile().getAbsoluteFile());
                         System.out.println(istream.toString());
-
                         if(fc.showSaveDialog(null) == fc.APPROVE_OPTION) {
-
                             byte[] data = new byte[20480000];
                             int len, count = 0;
-
                             OutputStream ostream = new FileOutputStream(fc.getSelectedFile().getAbsoluteFile());
                             GZIPOutputStream gzipOutputStream = new GZIPOutputStream(ostream);
-
                             while((len = istream.read(data)) != -1){
                                 gzipOutputStream.write(data,0, len);
                                 count += len;
                                 System.out.println("packed " + count);
-
                             }
-
                             istream.close();
                             gzipOutputStream.finish();
                             gzipOutputStream.close();
-
                         }
-
                     } catch (FileNotFoundException e) {
                         e.printStackTrace();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-
                 }
             }
             @Override
             public void btn02Clicked() {
                 System.out.println("btn02 Clicked");
                 JFileChooser fc = new JFileChooser();
-
                 if(fc.showOpenDialog(null) == fc.APPROVE_OPTION) {
-
                     try {
                         InputStream istream = new FileInputStream(fc.getSelectedFile().getAbsoluteFile());
                         GZIPInputStream gzipInputStream = new GZIPInputStream(istream);
-
                         if(fc.showSaveDialog(null) == fc.APPROVE_OPTION) {
-
                             byte[] data = new byte[1024];
-
                             int len;
                             int count = 0;
                             OutputStream ostream = new FileOutputStream(fc.getSelectedFile().getAbsoluteFile());
                             while((len = gzipInputStream.read(data)) != -1 ) {
-
                                 ostream.write(data,0, len);
                             }
-
                             gzipInputStream.close();
                             ostream.close();
                         }
@@ -126,6 +106,4 @@ public class Zadanie42 extends JPanel {
         void btn01Clicked();
         void btn02Clicked();
     }
-
-
 }
